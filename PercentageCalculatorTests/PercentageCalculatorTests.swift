c8fd4c3e02e22f2736e108d65092d4b8611e53e1
Soner Güler <sonerguler93@gmail.com>
2017-03-17 11:53:29 +0300

    first

---
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -0,0 +1,42 @@
+//
+//  PercentageCalculatorTests.swift
+//  PercentageCalculatorTests
+//
+//  Created by Maxime Defauw on 03/02/16.
+//  Copyright © 2016 App Coda. All rights reserved.
+//
+
+import XCTest
+@testable import PercentageCalculator
+
+class PercentageCalculatorTests: XCTestCase {
+    
+    var vc: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        vc = storyboard.instantiateInitialViewController() as! ViewController
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testPercentageCalculator() {
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
+    }
+    
+    func testLabelValuesShowedProperly() {
+        let _ = vc.view
+        vc.updateLabels(80.0, 50.0, 40.0)
+        
+        XCTAssert(vc.numberLabel.text == "80.0","numberLabel doesn't show the right text")
+        XCTAssert(vc.percentageLabel.text == "50.0%","percentageLabel doesn't show the right text")
+        XCTAssert(vc.resultLabel.text == "40.0","resultLabel doesn't show the right text")
+    }
+    
+}
